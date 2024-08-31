6d60facfb30249f8295da1a508dfe51bc4f6d407
Hector Gonzalez <gorozco.hector@gmail.com>
2024-08-31 14:07:17 -0600

    Add footer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig } from "vite";
 import { configDefaults } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,13 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
   build: {
     target: "esnext",
   },
