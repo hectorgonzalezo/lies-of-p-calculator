4151e973dfe52f1ab9e473c8b149ce037b72a282
Hector Gonzalez <gorozco.hector@gmail.com>
2024-08-31 13:47:27 -0600

    Initialize project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./testConfig.ts"],
+    coverage: {
+      provider: "v8",
+      exclude: [...configDefaults.coverage.exclude, "*.config.js"],
+      all: true,
+    },
+  },
+  plugins: [react()],
+  build: {
+    target: "esnext",
+  },
+});
