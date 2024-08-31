5a8828686448c57da7fc30043427a018ed841795
Hector Gonzalez <gorozco.hector@gmail.com>
2024-08-31 14:33:32 -0600

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/app.scss';
 
 function App() {
 
   return (
-    <body>
+    <body id="app">
+      <Header />
       <Footer projectName="lies-of-p-calculator" />
     </body>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import '../styles/headerStyle.scss';
+
+function Header(): JSX.Element {
+
+  return (
+    <header>
+      <h1>Lies of P Calculator</h1>
+
+      <h2>Lies of P version 1.5</h2>
+    </header>
+  );
+}
+
+export default Header;
